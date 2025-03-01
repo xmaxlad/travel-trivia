88c45a95e044d9d0b731bddeef1308c4ef21a405
Kamal <kamalproacc@gmail.com>
2025-03-01 20:35:03 +0530

    fixed create_question route

--- app/api/create-question/route.ts
+++ app/api/create-question/route.ts
@@ -1,37 +1,44 @@
 import {NextResponse} from 'next/server' 
 import prisma from '@/lib/prisma'
 import { random_index_insert } from '@/lib/utils'
+import random from 'random'
+import type { Destination } from '@prisma/client'
 
 //Generate a new random trivia question. 
 export async function GET(){
     try{
         const destinations_count = await prisma.destination.count()
-        const random_index = Math.floor(Math.random() * destinations_count)
+        const random_index = random.int(0,destinations_count)
+        const random_index_arr = [random.int(0,destinations_count),random.int(0,destinations_count),random.int(0,destinations_count)]
+        const other_destinations : Destination[] = [] 
 
-        const destination = await prisma.destination.findMany({
-            skip: random_index,
-            take : 1,
-        })
-
-        const other_destinations = await prisma.destination.findMany({
+        const destination = await prisma.destination.findUnique({
             where:{
-                NOT :{
-                    id : destination[0].id 
-                }
-            },
-            take : 3,
-            orderBy:{
-                id : 'asc'
+                id : random_index 
+            }
+        }) as Destination
+
+        random_index_arr.forEach(async(index)=>{
+            if(other_destinations.length < 3){
+                const des = await prisma.destination.findUnique({
+                    where:{
+                        NOT : {
+                            id : destination.id 
+                        },
+                        id : index,
+                    }
+                })
+                other_destinations.push(des as Destination)
             }
         })
 
         const trivia_question = {
-            id : destination[0].id,
-            clues : destination[0].clues,
-            fun_fact : destination[0].fun_fact,
-            trivia : destination[0].trivia,
-            options : random_index_insert(other_destinations,destination[0]).map(des => des.city), 
-            answer : destination[0].city, 
+            id : destination.id,
+            clues : destination.clues,
+            fun_fact : destination.fun_fact,
+            trivia : destination.trivia,
+            options : random_index_insert(other_destinations,destination).map(des => des.city), 
+            answer : destination.city, 
         }
 
         return NextResponse.json(trivia_question)
